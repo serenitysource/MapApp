7e2189a627e758b81f769b9f7afa6389769f9f58
Adi <53587881+adi-foxtel@users.noreply.github.com>
2025-12-15 10:55:34 +1100

    fix spils

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,10 @@
 
 import Vue from 'vue';
-<<<<<<< HEAD
 import store from './store';
-import MapView from './MapView.vue';
-
-new Vue({
-  store,
-  render: h => h(MapView)
-=======
 import App from './App.vue';
 import 'leaflet/dist/leaflet.css';
 
 new Vue({
+  store,
   render: h => h(App)
->>>>>>> 9f564a13e6f0aab8efc1c573e7779e021f9fef34
 }).$mount('#app');
